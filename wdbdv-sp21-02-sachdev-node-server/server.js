cca469e1905965978da87f1fb4f3aab1d63aff02
nadkarniak <nadkarni.ak@gmail.com>
2021-04-17 15:54:09 -0400

    Initial

---
+++ wdbdv-sp21-02-sachdev-node-server/server.js
@@ -0,0 +1,25 @@
+// TODO: visit https://expressjs.com/
+const express = require('express')
+const app = express()
+
+// Configures CORS
+app.use(function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers',
+        'Content-Type, X-Requested-With, Origin');
+    res.header('Access-Control-Allow-Methods',
+        'GET, POST, PUT, PATCH, DELETE, OPTIONS');
+    next();
+});
+
+
+const demos = require('./controllers/demos-controller')
+demos(app)
+
+// const quizzesController = require('./controllers/quizzes-controller')
+// quizzesController(app)
+
+require('./controllers/quizzes-controller')(app)
+require('./controllers/questions-controller')(app)
+
+app.listen(4000)
